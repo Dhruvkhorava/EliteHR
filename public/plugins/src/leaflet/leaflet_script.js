febf1aa288ea5e56258f396c2a20742a13c43175
Dhruvkhorava <dkhorava@icrafttechnologies.com>
2026-03-23 18:11:08 +0530

    fix script

--- public/plugins/src/leaflet/leaflet_script.js
+++ public/plugins/src/leaflet/leaflet_script.js
@@ -1,48 +1,55 @@
 /**
- * 
- *  Basic 
+ *
+ *  Basic
  */
 
-var map = L.map('basic-map').setView([51.505, -0.09], 13);
+var map = L.map("basic-map").setView([51.505, -0.09], 13);
 
-var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+    attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/streets-v11',
+    id: "mapbox/streets-v11",
     tileSize: 512,
-    zoomOffset: -1
+    zoomOffset: -1,
 }).addTo(map);
 
 var marker = L.marker([51.5, -0.09]).addTo(map);
 
-
 /**
- * 
+ *
  *  Panes
  */
 
-var map = L.map('map-panes');
+var map = L.map("map-panes");
 
-map.createPane('labels');
+map.createPane("labels");
 
 // This pane is above markers but below popups
-map.getPane('labels').style.zIndex = 650;
+map.getPane("labels").style.zIndex = 650;
 
 // Layers in this pane are non-interactive and do not obscure mouse/touch events
-map.getPane('labels').style.pointerEvents = 'none';
+map.getPane("labels").style.pointerEvents = "none";
 
-var cartodbAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>';
+var cartodbAttribution =
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>';
 
-var positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
-    attribution: cartodbAttribution
-}).addTo(map);
+var positron = L.tileLayer(
+    "https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png",
+    {
+        attribution: cartodbAttribution,
+    },
+).addTo(map);
 
-var positronLabels = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', {
-    attribution: cartodbAttribution,
-    pane: 'labels',
-    color: '#00ab55'
-}).addTo(map);
+var positronLabels = L.tileLayer(
+    "http://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png",
+    {
+        attribution: cartodbAttribution,
+        pane: "labels",
+        color: "#00ab55",
+    },
+).addTo(map);
 
 /* global euCountries */
 var geojson = L.geoJson(euCountries).addTo(map);
@@ -51,63 +58,78 @@ geojson.eachLayer(function (layer) {
     layer.bindPopup(layer.feature.properties.name);
 });
 
-map.setView({lat: 47.040182144806664, lng: 9.667968750000002}, 4);
-
-    
+map.setView({ lat: 47.040182144806664, lng: 9.667968750000002 }, 4);
 
 /**
- * 
+ *
  *  Interactive Choropleth Map
  */
-    
-var interactiveMap = L.map('interactive-map').setView([37.8, -96], 4);
 
-var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=YOUR_MAPBOX_ACCESS_TOKEN', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/light-v9',
-    tileSize: 512,
-    zoomOffset: -1
-}).addTo(interactiveMap);
+var interactiveMap = L.map("interactive-map").setView([37.8, -96], 4);
 
+var tiles = L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=YOUR_MAPBOX_ACCESS_TOKEN",
+    {
+        maxZoom: 18,
+        attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: "mapbox/light-v9",
+        tileSize: 512,
+        zoomOffset: -1,
+    },
+).addTo(interactiveMap);
 
 // control that shows state info on hover
 var info = L.control();
 
 info.onAdd = function (interactiveMap) {
-    this._div = L.DomUtil.create('div', 'info');
+    this._div = L.DomUtil.create("div", "info");
     this.update();
     return this._div;
 };
 
 info.update = function (props) {
-    this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
-        '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>' : 'Hover over a state');
+    this._div.innerHTML =
+        "<h4>US Population Density</h4>" +
+        (props
+            ? "<b>" +
+              props.name +
+              "</b><br />" +
+              props.density +
+              " people / mi<sup>2</sup>"
+            : "Hover over a state");
 };
 
 info.addTo(interactiveMap);
 
-
 // get color depending on population density value
 function getColor(d) {
-    return d > 1000 ? '#800026' :
-        d > 500  ? '#BD0026' :
-        d > 200  ? '#E31A1C' :
-        d > 100  ? '#FC4E2A' :
-        d > 50   ? '#FD8D3C' :
-        d > 20   ? '#FEB24C' :
-        d > 10   ? '#FED976' : '#FFEDA0';
+    return d > 1000
+        ? "#800026"
+        : d > 500
+          ? "#BD0026"
+          : d > 200
+            ? "#E31A1C"
+            : d > 100
+              ? "#FC4E2A"
+              : d > 50
+                ? "#FD8D3C"
+                : d > 20
+                  ? "#FEB24C"
+                  : d > 10
+                    ? "#FED976"
+                    : "#FFEDA0";
 }
 
 function style(feature) {
     return {
         weight: 2,
         opacity: 1,
-        color: 'white',
-        dashArray: '3',
+        color: "white",
+        dashArray: "3",
         fillOpacity: 0.7,
-        fillColor: getColor(feature.properties.density)
+        fillColor: getColor(feature.properties.density),
     };
 }
 
@@ -116,9 +138,9 @@ function highlightFeature(e) {
 
     layer.setStyle({
         weight: 5,
-        color: '#666',
-        dashArray: '',
-        fillOpacity: 0.7
+        color: "#666",
+        dashArray: "",
+        fillOpacity: 0.7,
     });
 
     if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
@@ -143,24 +165,24 @@ function onEachFeature(feature, layer) {
     layer.on({
         mouseover: highlightFeature,
         mouseout: resetHighlight,
-        click: zoomToFeature
+        click: zoomToFeature,
     });
 }
 
 /* global statesData */
 geojson = L.geoJson(statesData, {
     style: style,
-    onEachFeature: onEachFeature
+    onEachFeature: onEachFeature,
 }).addTo(interactiveMap);
 
-interactiveMap.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
+interactiveMap.attributionControl.addAttribution(
+    'Population data &copy; <a href="http://census.gov/">US Census Bureau</a>',
+);
 
-
-var legend = L.control({position: 'bottomright'});
+var legend = L.control({ position: "bottomright" });
 
 legend.onAdd = function (interactiveMap) {
-
-    var div = L.DomUtil.create('div', 'info legend');
+    var div = L.DomUtil.create("div", "info legend");
     var grades = [0, 10, 20, 50, 100, 200, 500, 1000];
     var labels = [];
     var from, to;
@@ -170,11 +192,15 @@ legend.onAdd = function (interactiveMap) {
         to = grades[i + 1];
 
         labels.push(
-            '<i style="background:' + getColor(from + 1) + '"></i> ' +
-            from + (to ? '&ndash;' + to : '+'));
+            '<i style="background:' +
+                getColor(from + 1) +
+                '"></i> ' +
+                from +
+                (to ? "&ndash;" + to : "+"),
+        );
     }
 
-    div.innerHTML = labels.join('<br>');
+    div.innerHTML = labels.join("<br>");
     return div;
 };
 
